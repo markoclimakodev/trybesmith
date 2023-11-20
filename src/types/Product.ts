d22d7fe40fc36157e18a87dd23efaaee2cee7c09
markoclimako <mkclimako@gmail.com>
2023-11-20 17:06:36 -0300

    feat: add product and service response types

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,6 +1,10 @@
+import { Optional } from 'sequelize';
+
 export type Product = {
   id: number;
   name: string;
   price: string;
   orderId: number;
 };
+
+export type ProductInputtableTypes = Optional<Product, 'id'>;
