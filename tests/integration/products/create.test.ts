e3fc2dc7e5096344b722cda71f89c273d400ecd4
markoclimako <mkclimako@gmail.com>
2023-11-20 17:07:09 -0300

    test: add tests for  Products Service

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,22 @@
-import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import Sinon from 'sinon';
+import app from '../../../src/app';
+import ProductModel from '../../../src/database/models/product.model';
+import { prodcutFromDb, validProductBody } from '../../mocks/products.mock';
 
 chai.use(chaiHttp);
 
-describe('POST /products', function () { 
-  beforeEach(function () { sinon.restore(); });
+describe('ProductsService', function () {
+  beforeEach(function () { Sinon.restore(); });
 
-});
+ it('should register a product', async function() {
+  const productSimulated = ProductModel.build(prodcutFromDb)
+  Sinon.stub(ProductModel, 'create').resolves(productSimulated)
+
+  const response = await chai.request(app).post('/products').send(validProductBody)
+  expect(response.status).to.be.equal(201)
+  expect(response.body).to.have.keys(['id','name','price','orderId'])
+})
+
+})
---
+++ tests/mocks/products.mock.ts
@@ -0,0 +1,14 @@
+import { ProductInputtableTypes } from '../../src/types/Product';
+
+export const validProductBody: ProductInputtableTypes = {
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  orderId: 4
+};
+
+export const prodcutFromDb = {
+	id: 6,
+	name: "Martelo de Thor",
+	price: "30 peças de ouro",
+	orderId: 4
+}
