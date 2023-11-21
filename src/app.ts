ad549dfc4eb179d7df9410fb1c6e548c2819c684
markoclimako <mkclimako@gmail.com>
2023-11-21 03:26:17 -0300

    feat: add auth router

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import authRouter from './routers/auth.routes';
 import ordersRouter from './routers/orders.routes';
 import productsRouter from './routers/products.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(authRouter);
 app.use(productsRouter);
 app.use(ordersRouter);
 
